2201f024d9b7488b8649c3055a35be0186d6c0eb
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-03 17:46:07 +0100

    Input Stream(Byte Array One(2))

---
+++ src/main/java/org/example/ByteArrayInputStreamTestZero.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class ByteArrayInputStreamTestZero {
+    public static void main(String[] args) throws IOException {
+        byte[] bytes = {1, 3, 5, 7, 9};
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+
+        while (bais.available() > 0) {
+            byte currentByte = (byte) bais.read();
+            System.out.println(currentByte);
+        }
+
+        bais.close();
+    }
+}
