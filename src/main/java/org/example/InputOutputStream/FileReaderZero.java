cabf9449379197aa2d349fea5c9e09ed9f4b479c
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-06 21:08:53 +0100

    file Reader, first

---
+++ src/main/java/org/example/InputOutputStream/FileReaderZero.java
@@ -0,0 +1,16 @@
+package org.example.InputOutputStream;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileReaderZero {
+    public static void writeFileWithOutputStream() {
+        String text = "Hello from Output Stream";
+        try(FileOutputStream fileOutputStream = new FileOutputStream("test.txt")) {
+            byte[] buffer = text.getBytes();
+            fileOutputStream.write(buffer, 0, buffer.length);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
