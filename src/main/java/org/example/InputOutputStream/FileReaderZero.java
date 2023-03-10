53b0ca68a68ee11618552ab28d823fbd6dc228e8
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-10 23:31:20 +0100

    second File Reader

--- src/main/java/org/example/InputOutputStream/FileReaderZero.java
+++ src/main/java/org/example/InputOutputStream/FileReaderZero.java
@@ -2,16 +2,23 @@ package org.example.InputOutputStream;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class FileReaderZero {
-    public static void readFile(){
+    public static void readFileWithBuffer(){
         try(FileReader reader = new FileReader("test.txt")){
+            char[] buff = new char[256];
             int c;
-            while ((c == reader.read() != -1)){
-                System.out.println((char) c);
+            while((c == reader.read(buff) > 0)){
+                if (c < 256){
+                    System.out.println(Arrays.copyOf(buff, c));
+                }
+                System.out.println(Arrays.toString(buff));
             }
-        }catch(IOException e){
-            System.out.println(e.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+        readFileWithBuffer();
     }
 }
